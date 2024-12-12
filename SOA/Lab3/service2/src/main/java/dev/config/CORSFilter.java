cfa1d32661d0eb6c41b9300880256a02aa416295
netwid <me@netwid.dev>
2024-12-12 19:12:16 +0300

    Lab3

---
+++ SOA/Lab3/service2/src/main/java/dev/config/CORSFilter.java
@@ -0,0 +1,24 @@
+package dev.config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter("/*")
+public class CORSFilter implements Filter {
+    public void init(FilterConfig filterConfig) throws ServletException {}
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse res = (HttpServletResponse) response;
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+        chain.doFilter(request, response);
+    }
+
+    public void destroy() {}
+}
---
+++ SOA/Lab3/service2/src/main/java/dev/dto/Percentage.java
@@ -0,0 +1,12 @@
+package dev.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+public class Percentage {
+    int value;
+}
