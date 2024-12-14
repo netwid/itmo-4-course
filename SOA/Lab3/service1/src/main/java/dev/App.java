c83247e2e68ee45ff18ea2bede2b6af3e25761f1
GrigAnd <andrey.grigorev03@gmail.com>
2024-12-14 07:31:26 +0300

    consul 1/2

--- SOA/Lab3/service1/src/main/java/dev/App.java
+++ SOA/Lab3/service1/src/main/java/dev/App.java
@@ -2,9 +2,10 @@ package dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
